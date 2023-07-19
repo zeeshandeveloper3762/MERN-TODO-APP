09c1e71811adc6bb4c947dee117779d36afd9505
gulshan07dev <gulshanku9135@gmail.com>
2023-07-19 16:06:32 +0530

    update app.js in backend because of some error

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const userRouter = require('./route/userRoute.js');
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
+app.use(cors({origin: [process.env.CLIENT_URL], credentials: true}))
 app.use(cookieParser());
 
 app.use('/', userRouter);
